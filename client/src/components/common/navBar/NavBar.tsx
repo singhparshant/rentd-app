1145d23d86d58ce46727bfba2e895ff268e872de
Anoir Trabelsi <anoir@Anoirs-Air.fritz.box>
2022-06-13 00:04:22 +0200

    implemented navbar of unauthenticated app

--- client/src/components/common/navBar/NavBar.tsx
+++ client/src/components/common/navBar/NavBar.tsx
@@ -2,24 +2,30 @@ import React from "react";
 import "./NavBar.css"
 import logo from "../../../assets/logo.png"
 import { Link } from "react-router-dom"
-
+import { ReactComponent as CartIcon } from "../../../assets/icons/cart.svg"
+import Button from "@mui/material/Button"
+import {ReactComponent as SearchIcon} from "../../../assets/icons/search.svg"
 interface NavbarProps {
   mode?: "customer" | "supplier" | "admin"
 };
 
 const NavBar = ({ mode }: NavbarProps) => {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <Link to="/">
         <img className="logo" src={logo} alt="logo" />
       </Link>
       <div className="searchField">
         <input className="searchInput" type="text" placeholder="Search for products" />
-        <svg className="searchIcon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
+        <SearchIcon/>
       </div>
-    </div>
+      <Link to="/cart">
+        <CartIcon />
+      </Link>
+      <Link to="/register">
+        <Button children="Join Us" />
+      </Link>
+    </nav>
   )
 };
 
