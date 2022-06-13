0a26ac0d39e1fb7dc5b43de208a643d1f1484fc3
Pablo <pablo.rodrigo.valero@gmail.com>
2022-06-13 10:52:05 +0200

    Create first product logic

---
+++ server/models/order.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let orderSchema = new Schema(
+  {
+    customerId: {
+        type: Schema.Types.ObjectId, ref: "user"
+    },
+    amount: Number,
+    monthlyPrice: Number,
+    orderItems:[{
+        productID:{
+            type: Schema.Types.ObjectId, ref: "product"
+        },
+        quantity: Number,
+        rentalDuration: Number
+    }],
+    paymentMethod: String,
+    paymentID: {
+        type: Schema.Types.ObjectId, ref: "payment"
+    },
+    deliveryID: {
+        type: Schema.Types.ObjectId, ref: "delivery"
+    },
+    Status: String,
+  },
+  { timestamps: true }
+);
+
+let order = mongoose.model("order", orderSchema);
+
+module.exports = order;
--- server/routes/products.js
+++ server/routes/products.js
@@ -14,13 +14,41 @@ const router = express.Router();
  */
 router.get("/", async (req, res) => {
   try {
-    console.log("Hello");
     let products = await Product.find();
     res.status(200).json(products);
   } catch (err) {
-    res.status(400).json(err.message);
+    res.status(400).res.status(400).json(err.message);
   }
 });
+
+function getProduct(name) {
+  try{
+    let products = await Product.getProduct({"name": name});
+    return json(products);
+  }
+  catch (err){
+    return res.status(400).json(err.message);
+  }
+}
+function getAllProductsSupplier(supplierIdArg){
+  try{
+    let products = await Product.find({"supplierId": supplierIdArg});
+    return json(products);
+  }
+  catch (err){
+    return res.status(400).json(err.message);
+  }
+}
+
+function productFilterPrice(min, max){
+  try{
+    let products = await Product.find({"monthlyPrice": {$gt: min, $lt: max}});
+    return json(products);
+  }
+  catch (err){
+    return res.status(400).json(err.message);
+  }
+}
 
 
 /**
