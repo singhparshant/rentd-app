3cf0945c4fe498b29357095304d4423944da9039
Anoir Trabelsi <anoir@Anoirs-MacBook-Air.local>
2022-07-13 18:40:12 +0200

    product suggestions

--- client/src/components/common/productDetails/ProductDetailsScreen.tsx
+++ client/src/components/common/productDetails/ProductDetailsScreen.tsx
@@ -1,4 +1,5 @@
 import {
+  Card,
   FormControl,
   InputLabel,
   MenuItem,
@@ -48,7 +49,7 @@ export default function ProductDetailsScreen() {
   const [totalAmount, setTotalAmount] = useState(0);
   const { width } = useViewport();
   const [rentalDuration, setRentalDuration] = useState<number>(0);
-  const [suggestedProducts, setSuggestedProducts] = useState<any>({});
+  const [suggestedProducts, setSuggestedProducts] = useState<any>([]);
   const [userRating, setUserRating] = useState(0);
   const { state } = location as LocationInterface;
   const product: Product = state.fromProductsPage;
@@ -67,6 +68,8 @@ export default function ProductDetailsScreen() {
     return options;
   };
 
+  console.log("suggestions", suggestedProducts);
+
   //TODO: suggested products (additional feature)
   useEffect(() => {
     const getProductsByCategory = async () => {
@@ -75,7 +78,7 @@ export default function ProductDetailsScreen() {
           categories: [`${product.category}`],
         },
       });
-      setSuggestedProducts(response);
+      setSuggestedProducts(response.data.data);
     };
     getProductsByCategory();
   }, []);
@@ -100,101 +103,119 @@ export default function ProductDetailsScreen() {
   };
 
   return (
-    <div
-      className="productdetailsScreen"
-      style={{
-        display: width > breakpoint ? "flex" : "block",
-        flexWrap: "wrap",
-      }}
-    >
-      <div className="carouselContainer">
-        <Carousel1>
-          {product.productImages.map((image, idx) => {
-            return (
-              <img
-                src={`data:image/png;base64,` + image}
-                style={{
-                  margin: "25px",
-                  maxHeight: 350,
-                  maxWidth: 350,
-                  width: "auto",
-                  height: "auto",
-                  borderRadius: "7px",
-                }}
-                key={idx}
-                alt="Could not load."
-              />
-            );
-          })}
-        </Carousel1>
-        <div className="ratingContainer">
-          <div style={{ marginRight: 5 }}>Rate:</div>
-          <Rating
-            name="half-rating"
-            precision={0.5}
-            value={userRating}
-            onChange={(e, newValue) => handleRating(newValue || 0)}
-          />
-        </div>
-      </div>
-      <div className="detailsContainer">
-        <Typography variant="h4">{product.name}</Typography>
-        <br />
-        <Typography variant="h5" style={{ color: "green" }}>
-          €{product.monthlyPrice}/month &nbsp;&nbsp;
-        </Typography>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <Rating
-            name="half-rating"
-            value={product.avgRating}
-            precision={0.5}
-            readOnly
-          />
-          <span style={{ marginLeft: 5 }}>({product.numberRatings})</span>
-        </div>
-
-        <Typography style={{ marginTop: 20 }} variant="body1">
-          {product.description}
-        </Typography>
-      </div>
-      <div className="functionalitiesContainer">
-        <div>
-          <label style={{ fontSize: "20px" }}>Total Amount: &nbsp;</label>
-          <strong style={{ color: "green", fontSize: 30 }}>
-            €
-            {product.monthlyPrice *
-              rentalDuration *
-              (1 - 0.01 * product.discount)}
-          </strong>
-          <br />
-          <label style={{ fontSize: "10px" }}>
-            *Amount will be deducted on monthly basis
-          </label>
-          <br />
-          <br />
-        </div>
-        <br />
-        <FormControl variant="outlined" sx={{ width: "70%" }}>
-          <InputLabel>Rental Duration</InputLabel>
-          <Select
-            value={rentalDuration}
-            label={"Rental Duration"}
-            onChange={(e: any) => setRentalDuration(e.target.value)}
-            name="Rental Duration"
-          >
-            {getOptions().map((option: Ioption, index) => {
+    <>
+      <div
+        className="productdetailsScreen"
+        style={{
+          display: width > breakpoint ? "flex" : "block",
+          flexWrap: "wrap",
+        }}
+      >
+        <div className="carouselContainer">
+          <Carousel1>
+            {product.productImages.map((image, idx) => {
               return (
-                <MenuItem key={index} value={option.value}>
-                  {option.label}
-                </MenuItem>
+                <img
+                  src={`data:image/png;base64,` + image}
+                  style={{
+                    margin: "25px",
+                    maxHeight: 350,
+                    maxWidth: 350,
+                    width: "auto",
+                    height: "auto",
+                    borderRadius: "7px",
+                  }}
+                  key={idx}
+                  alt="Could not load."
+                />
               );
             })}
-          </Select>
-        </FormControl>
-        <div className="button" onClick={handleAddToCart}>
-          Add to Cart
+          </Carousel1>
+          <div className="ratingContainer">
+            <div style={{ marginRight: 5 }}>Rate:</div>
+            <Rating
+              name="half-rating"
+              precision={0.5}
+              value={userRating}
+              onChange={(e, newValue) => handleRating(newValue || 0)}
+            />
+          </div>
+        </div>
+        <div className="detailsContainer">
+          <Typography variant="h4">{product.name}</Typography>
+          <br />
+          <Typography variant="h5" style={{ color: "green" }}>
+            €{product.monthlyPrice}/month &nbsp;&nbsp;
+          </Typography>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Rating
+              name="half-rating"
+              value={product.avgRating}
+              precision={0.5}
+              readOnly
+            />
+            <span style={{ marginLeft: 5 }}>({product.numberRatings})</span>
+          </div>
+
+          <Typography style={{ marginTop: 20 }} variant="body1">
+            {product.description}
+          </Typography>
+        </div>
+        <div className="functionalitiesContainer">
+          <div>
+            <label style={{ fontSize: "20px" }}>Total Amount: &nbsp;</label>
+            <strong style={{ color: "green", fontSize: 30 }}>
+              €
+              {product.monthlyPrice *
+                rentalDuration *
+                (1 - 0.01 * product.discount)}
+            </strong>
+            <br />
+            <label style={{ fontSize: "10px" }}>
+              *Amount will be deducted on monthly basis
+            </label>
+            <br />
+            <br />
+          </div>
+          <br />
+          <FormControl variant="outlined" sx={{ width: "70%" }}>
+            <InputLabel>Rental Duration</InputLabel>
+            <Select
+              value={rentalDuration}
+              label={"Rental Duration"}
+              onChange={(e: any) => setRentalDuration(e.target.value)}
+              name="Rental Duration"
+            >
+              {getOptions().map((option: Ioption, index) => {
+                return (
+                  <MenuItem key={index} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <div className="button" onClick={handleAddToCart}>
+            Add to Cart
+          </div>
         </div>
       </div>
-    </div>
+      <div className="suggestions">
+        Suggested products:
+        {suggestedProducts?.map((product: Product) => (
+          <Card
+            sx={{
+              margin: "8px",
+              width: "350px",
+              transition: "transform 0.15s ease-in-out",
+              "&:hover": { transform: "scale3d(1.05, 1.05, 1)" },
+              position: "relative",
+            }}
+          >
+            {product.name}
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
