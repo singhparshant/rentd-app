deaf74719dbf426d0bd8ad0c6d9c08b9320c5e63
Anoir Trabelsi <anoir@Anoirs-Air.fritz.box>
2022-07-20 18:33:10 +0200

    persist cart to local storage

--- client/src/zustand/useCart.tsx
+++ client/src/zustand/useCart.tsx
@@ -1,66 +1,69 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 import axiosInstance from "../api/axios";
 import { OrderItem } from "../components/common/interfaces/Interfaces";
-const useCart = create((set, get) => ({
-  cart: [],
-  setCart: async (id: any) => {
-    //TODO
-    const shoppingCart = await axiosInstance.get("/shoppingCarts", id);
-    set(() => ({
-      cart: shoppingCart.data.data,
-    }));
-  },
-  addItemToCart: (item: OrderItem) => AddItemToCart(item, get, set),
-  removeItem: (item: any) => {
-    set((state: any) => ({
-      cart: state.cart.filter((el: OrderItem) => el._id !== item._id),
-    }));
-  },
-  emptyCart: () => set({ cart: [] }),
+const useCart = create(
+  persist((set, get) => ({
+    cart: [],
+    setCart: async (id: any) => {
+      //TODO
+      const shoppingCart = await axiosInstance.get("/shoppingCarts", id);
+      set(() => ({
+        cart: shoppingCart.data.data,
+      }));
+    },
+    addItemToCart: (item: OrderItem) => AddItemToCart(item, get, set),
+    removeItem: (item: any) => {
+      set((state: any) => ({
+        cart: state.cart.filter((el: OrderItem) => el._id !== item._id),
+      }));
+    },
+    emptyCart: () => set({ cart: [] }),
 
-  decrementItemQuantity: (item: OrderItem) => {
-    set((state: any) => ({
-      cart: state.cart.map((el: OrderItem) => {
-        if (el._id === item._id) {
-          return {
-            ...el,
-            quantity: item.quantity - 1,
-          };
-        } else {
-          return el;
-        }
-      }),
-    }));
-  },
-  incrementItemDuration: (item: OrderItem) => {
-    set((state: any) => ({
-      cart: state.cart.map((el: OrderItem) => {
-        if (el._id === item._id) {
-          return {
-            ...el,
-            duration: item.duration + 1,
-          };
-        } else {
-          return el;
-        }
-      }),
-    }));
-  },
-  decrementItemDuration: (item: OrderItem) => {
-    set((state: any) => ({
-      cart: state.cart.map((el: OrderItem) => {
-        if (el._id === item._id) {
-          return {
-            ...el,
-            duration: item.duration - 1,
-          };
-        } else {
-          return el;
-        }
-      }),
-    }));
-  },
-}));
+    decrementItemQuantity: (item: OrderItem) => {
+      set((state: any) => ({
+        cart: state.cart.map((el: OrderItem) => {
+          if (el._id === item._id) {
+            return {
+              ...el,
+              quantity: item.quantity - 1,
+            };
+          } else {
+            return el;
+          }
+        }),
+      }));
+    },
+    incrementItemDuration: (item: OrderItem) => {
+      set((state: any) => ({
+        cart: state.cart.map((el: OrderItem) => {
+          if (el._id === item._id) {
+            return {
+              ...el,
+              duration: item.duration + 1,
+            };
+          } else {
+            return el;
+          }
+        }),
+      }));
+    },
+    decrementItemDuration: (item: OrderItem) => {
+      set((state: any) => ({
+        cart: state.cart.map((el: OrderItem) => {
+          if (el._id === item._id) {
+            return {
+              ...el,
+              duration: item.duration - 1,
+            };
+          } else {
+            return el;
+          }
+        }),
+      }));
+    },
+  }))
+);
 
 const AddItemToCart = (item: OrderItem, get: any, set: any) => {
   let cart = get().cart;
